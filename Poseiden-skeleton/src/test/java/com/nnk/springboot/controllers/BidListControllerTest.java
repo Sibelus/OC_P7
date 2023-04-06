8bd5cd01f70fa2f0f2ef5abe1614388aa25b1ab9
4H0 <ggaetan@proton.me>
2023-04-06 10:10:23 +0200

    Create BidListControllerTest

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/BidListControllerTest.java
@@ -0,0 +1,198 @@
+package com.nnk.springboot.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Transactional
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class BidListControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+
+    /* ------- home() ------- */
+    @WithMockUser(value = "admin")
+    @Test
+    public void testGetHome() throws Exception {
+        mockMvc.perform(get("/bidList/list")).andExpect(status().isOk());
+    }
+
+
+    /* ------- addBidForm() ------- */
+    @WithMockUser(value = "admin")
+    @Test
+    public void testGetAddBidForm() throws Exception {
+        mockMvc.perform(get("/bidList/add")).andExpect(status().isOk());
+    }
+
+
+    /* ------- validate() ------- */
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostValidate() throws Exception {
+        mockMvc.perform(post("/bidList/validate")
+                        .param("account", "account")
+                        .param("type", "type")
+                        .param("bidQuantity", "12")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/bidList/list"));
+    }
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostValidate_WithEmptyAccount() throws Exception {
+        mockMvc.perform(post("/bidList/validate")
+                        .param("account", "")
+                        .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeHasFieldErrorCode("bidList", "account", "NotBlank"));
+    }
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostValidate_WithEmptyType() throws Exception {
+        mockMvc.perform(post("/bidList/validate")
+                        .param("account", "account")
+                        .param("type", "")
+                        .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeHasFieldErrorCode("bidList", "type", "NotBlank"));
+    }
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostValidate_WithEmptyBidQuantity() throws Exception {
+        mockMvc.perform(post("/bidList/validate")
+                        .param("account", "account")
+                        .param("type", "type")
+                        .param("bidQuantity", "")
+                        .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeHasFieldErrorCode("bidList", "bidQuantity", "typeMismatch"));
+    }
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostValidate_WithLettersBidQuantity() throws Exception {
+        mockMvc.perform(post("/bidList/validate")
+                        .param("account", "account")
+                        .param("type", "type")
+                        .param("bidQuantity", "twenty two")
+                        .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeHasFieldErrorCode("bidList", "bidQuantity", "typeMismatch"));
+    }
+
+
+    /* ------- showUpdateForm() ------- */
+    @WithMockUser(value = "admin")
+    @Test
+    public void testGetshowUpdateForm() throws Exception {
+        mockMvc.perform(get("/bidList/update/100")).andExpect(status().isOk());
+    }
+
+    /*
+    @WithMockUser(value = "admin")
+    @Test
+    public void testGetshowUpdateForm_InvalidBidListId() throws Exception {
+        mockMvc.perform(get("/bidList/update/101")).andExpect(status().is5xxServerError());
+        //mockMvc.perform(get("/bidList/update/101")).andExpect(result -> assertTrue(result.getResolvedException() instanceof IllegalArgumentException));
+    }*/
+
+
+    /* ------- updateBid() ------- */
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostUpdateBid() throws Exception {
+        mockMvc.perform(post("/bidList/update/100")
+                        .param("account", "new account")
+                        .param("type", "new type")
+                        .param("bidQuantity", "42")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/bidList/list"));
+    }
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostUpdateBid_WithEmptyAccount() throws Exception {
+        mockMvc.perform(post("/bidList/update/100")
+                        .param("account", "")
+                        .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeHasFieldErrorCode("bidList", "account", "NotBlank"));
+    }
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostUpdateBid_WithEmptyType() throws Exception {
+        mockMvc.perform(post("/bidList/update/100")
+                        .param("account", "new account")
+                        .param("type", "")
+                        .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeHasFieldErrorCode("bidList", "type", "NotBlank"));
+    }
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostUpdateBid_WithEmptyBidQuantity() throws Exception {
+        mockMvc.perform(post("/bidList/update/100")
+                        .param("account", "new account")
+                        .param("type", "new type")
+                        .param("bidQuantity", "")
+                        .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeHasFieldErrorCode("bidList", "bidQuantity", "typeMismatch"));
+    }
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void testPostUpdateBid_WithLettersBidQuantity() throws Exception {
+        mockMvc.perform(post("/bidList/update/100")
+                        .param("account", "new account")
+                        .param("type", "new type")
+                        .param("bidQuantity", "Twenty two")
+                        .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeHasFieldErrorCode("bidList", "bidQuantity", "typeMismatch"));
+    }
+
+
+    /* ------- deleteBid() ------- */
+    @WithMockUser(value = "admin")
+    @Test
+    public void testGetDeleteBid() throws Exception {
+        mockMvc.perform(get("/bidList/delete/100"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/bidList/list"));
+    }
+}
