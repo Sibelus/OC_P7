6a52ac904de5629400c05dd2e47836482f2a678e
4H0 <ggaetan@proton.me>
2023-04-07 10:31:05 +0200

    Create SecurityTest

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/security/SecurityTest.java
@@ -0,0 +1,51 @@
+package com.nnk.springboot.security;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SecurityTest {
+
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        mockMvc.perform(get("/login")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUserLogin() throws Exception {
+        mockMvc.perform(formLogin("/login").user("admin").password("admin")).andExpect(authenticated());
+    }
+
+    @Test
+    public void testUserLoginFailed_WithWrongPassword() throws Exception {
+        mockMvc.perform(formLogin("/login").user("admin").password("wrongpassword")).andExpect(unauthenticated());
+    }
+}
